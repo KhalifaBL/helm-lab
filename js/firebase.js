afc2cfb8e03ae3dfff49bd671be6c344192ea695
Khalifa Ben Lazreg <khalifalazreg@gmail.com>
2020-12-18 02:57:27 +0100

    first

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAdc8fgzKLqeQeucxObaKy6JOqHjEHei4E",
+    authDomain: "k8s-training-labs.firebaseapp.com",
+    projectId: "k8s-training-labs",
+    storageBucket: "k8s-training-labs.appspot.com",
+    messagingSenderId: "215581639914",
+    appId: "1:215581639914:web:94628d44917b6549b2c393",
+    measurementId: "G-LV5Y2JY5T7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
